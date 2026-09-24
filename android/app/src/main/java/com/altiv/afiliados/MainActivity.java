d9cc5bb196471799ffeb58e8d5f3a63b78845d1f
Gustavojv065 <gustavojv065@gmail.com>
2026-09-23 22:47:50 -0400

    feat: add Android beta shell

---
+++ android/app/src/main/java/com/altiv/afiliados/MainActivity.java
@@ -0,0 +1,28 @@
+package com.altiv.afiliados;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private static final String APP_URL = "https://altiv-afiliados-ia-v2.vercel.app";
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(APP_URL);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
